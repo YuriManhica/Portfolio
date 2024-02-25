4f425e029f8d455ec01bccd00674a358eed7da69
YuriManhica <156890528+YuriManhica@users.noreply.github.com>
2024-02-25 21:04:02 +0200

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,3 +56,10 @@ document.addEventListener("scroll", function () {
   updateActiveLink("#project");
   updateActiveLink("#contact");
 });
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
